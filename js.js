da8cc16c9edb6490f9caebd5ad0d16ff87eb505f
Elliot Akande <elbo.lynch@gmail.com>
2021-11-01 19:01:40 +0000

    Add ability to clear the grid and set size

--- js.js
+++ js.js
@@ -1,16 +1,32 @@
-function createGrid(number){
+function createGrid(gridSize){
     const grid = document.querySelector("#grid");
-    for(let i = 0; i < number; i++){
+
+    clearGrid(grid);
+        
+    for(let i = 0; i < gridSize; i++){
         const row = document.createElement("div");
         row.classList.add("grid-row");
 
-        for(let x = 0; x < number; x++){
+        for(let x = 0; x < gridSize; x++){
         const item = document.createElement("div");
         item.classList.add("grid-item");
         row.appendChild(item);
         }
 
         grid.appendChild(row); 
+
+        const items = document.querySelectorAll(".grid-item");
+        items.forEach(item => item.addEventListener("mouseenter", function(e){
+        item.style.backgroundColor = "black";
+        }))
+    }
+}
+
+function clearGrid(grid){
+    var child = grid.lastElementChild; 
+    while (child) {
+        grid.removeChild(child);
+        child = grid.lastElementChild;
     }
 }
 
@@ -21,3 +37,12 @@ items.forEach(item => item.addEventListener("mouseenter", function(e){
     item.style.backgroundColor = "black";
 }))
 
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", function(e){
+    const gridSize = parseInt(prompt("Enter Size of New Grid: ", 16));
+    if(gridSize >= 1 && gridSize <= 100){
+        createGrid(gridSize);
+    } else {
+        alert("Invalid Input");
+    }
+})
