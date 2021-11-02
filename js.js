7be9966b26368a8d88b713253a6c8d62734b14cf
Elliot Akande <elbo.lynch@gmail.com>
2021-11-02 00:57:54 +0000

    Update styling to look far nicer

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
+let brushColor = "black";
+
 function createGrid(gridSize){
-    const grid = document.querySelector("#grid");
+    const grid = document.querySelector(".grid");
 
     clearGrid(grid);
         
@@ -17,28 +19,64 @@ function createGrid(gridSize){
 
         const items = document.querySelectorAll(".grid-item");
         items.forEach(item => item.addEventListener("mouseenter", function(e){
-        item.style.backgroundColor = "black";
+        item.style.backgroundColor = brushColor;
         }))
     }
 }
 
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('gridClear');
+  }
+
 function clearGrid(grid){
-    var child = grid.lastElementChild; 
+    let child = grid.lastElementChild; 
     while (child) {
         grid.removeChild(child);
         child = grid.lastElementChild;
     }
 }
 
+function changeBrushColor(e){
+    const color = this.id
+    switch(color){
+        case "red":
+            brushColor = "darkred";
+            break;
+        case "blue":
+            brushColor = "cadetblue";
+            break;
+        case "green":
+            brushColor = "forestgreen";
+            break;
+        case "yellow":
+            brushColor = "darkgoldenrod";
+            break;
+        case "white":
+            brushColor = "white";
+            break;
+        case "black":
+            brushColor = "black";
+            break;
+    }
+}
+
+function resizeGrid() {
+    const gridSize = parseInt(prompt("Enter Grid Size: ", 16));
+    if(gridSize >= 1 && gridSize <= 100){
+        const grid = document.querySelector(".grid");
+        grid.classList.add("gridClear");
+        createGrid(gridSize);
+    } 
+}
+
 createGrid(16);
 
-const items = document.querySelectorAll(".grid-item");
-items.forEach(item => item.addEventListener("mouseenter", function(e){
-    item.style.backgroundColor = "black";
-}))
-
 const clear = document.querySelector("#clear");
-clear.addEventListener("click", function(e){
-    const gridSize = parseInt(prompt("Enter Size of New Grid: ", 16));
-    if(gridSize >= 1 && gridSize <= 100) createGrid(gridSize);
-})
+clear.addEventListener("click", resizeGrid);
+
+const grid = document.querySelector(".grid");
+grid.addEventListener('transitionend', removeTransition);
+
+const colorButtons = document.querySelectorAll(".color-button");
+colorButtons.forEach(colorButton => colorButton.addEventListener("click", changeBrushColor));
